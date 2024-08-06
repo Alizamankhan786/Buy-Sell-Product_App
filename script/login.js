db14a7cd131d2b16625122a270f961788bca7916
Alizamankhan786 <az030366@gmail.com>
2024-08-06 10:48:24 -0700

    index file created

---
+++ script/login.js
@@ -0,0 +1,5 @@
+
+
+const form = document.querySelector(`#form`);
+const email = document.querySelector(`#email`);
+const password = document.querySelector(`#password`);
---
+++ script/register.js
@@ -0,0 +1,7 @@
+
+
+
+const form = document.querySelector(`#form`);
+const lastname = document.querySelector(`#lastname`);
+const email = document.querySelector(`#email`);
+const password = document.querySelector(`#password`);
